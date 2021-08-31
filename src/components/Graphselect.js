3a826eea68fa6f322558be755b011e2d9f9a9f03
Alok650 <alokp650@gmail.com>
2021-08-31 14:17:25 +0530

    dashboard

---
+++ src/components/Graphselect.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
+export default function Graphselector() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+        Select Graph
+      </Button>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Line Graph</MenuItem>
+        <MenuItem onClick={handleClose}>Bar Chart</MenuItem>
+        <MenuItem onClick={handleClose}>OHLC Graph</MenuItem>
+      </Menu>
+    </div>
+  );
+}
--- src/components/StocksGraph.jsx
+++ src/components/StocksGraph.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {Line} from 'react-chartjs-2';
 import * as zoom from 'chartjs-plugin-zoom'
 import { chartJsConfig, chartColors, chartDataset } from '../chartConfig.js'
+import Graphselector from './Graphselect'
 import { FormControl, MenuItem, Select, Card, CardContent} from '@material-ui/core';
 
 class StocksGraph extends React.Component {
@@ -81,13 +82,7 @@ class StocksGraph extends React.Component {
         </div>
         <div className='card-content'>
           <div className="graph-menu">
-          <FormControl>
-            <Select value = 'Graph'>
-              <MenuItem value= "linegraph">Bar chart</MenuItem>
-              <MenuItem value= "linegraph">Histogram</MenuItem>
-              <MenuItem value= "linegraph">Other</MenuItem>
-            </Select>
-          </FormControl>
+            <Graphselector/>
           </div>
           <p className='is-size-7 has-text-info'>
             {
